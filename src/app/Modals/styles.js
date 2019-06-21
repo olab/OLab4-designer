4ddf904ca896146c82dce2533085850a1375bdbd
Denis Bezak <denis.bezak@itrexgroup.com>
2019-06-21 14:49:33 +0300

    OLAB-88: Fix SO Picker UI & Add reload button
    - Fixed Search Logic;
    - Fixed scrollbar behavior;
    - Added reload button that fetches SOs;
    
    Change-Id: I1f98684532be3f620dd3db4a92bc71008470e7ac

--- src/app/Modals/styles.js
+++ src/app/Modals/styles.js
@@ -26,7 +26,7 @@ export const ModalHeader = styled.div`
   font-size: 32px;
   display: flex;
   color: ${colorBlackBlue};
-  justify-content: flex-start;
+  justify-content: flex-end;
   position: relative;
   padding: 0.5rem 1rem;
   border-bottom: 1px solid ${colorLightGrayWOpacity};
@@ -34,17 +34,15 @@ export const ModalHeader = styled.div`
 
   > h4 {
     margin: 0;
+    margin-right: auto;
     font-size: 20px;
   }
 
   > button {
-    position: absolute;
     cursor: pointer;
     background: transparent;
     border: none;
     outline: none;
     padding: 0;
-    top: 10px;
-    right: 10px;
   }
 `;
