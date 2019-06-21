7c14d736c7f4eb1a3a3580de36baefcc99594ec3
Denis Bezak <denis.bezak@itrexgroup.com>
2019-06-21 14:49:33 +0300

    OLAB-88: Fix SO Picker UI & Add reload button
    - Fixed Search Logic;
    - Fixed scrollbar behavior;
    - Added reload button that fetches SOs;
    
    Change-Id: Ica13bb5bea891fb457472113af2ca0ed683127d6

--- src/app/Modals/SOPicker/utils.js
+++ src/app/Modals/SOPicker/utils.js
@@ -1,6 +1,9 @@
-const getFilterCallback = (level, queryStr) => ({ name, scopeLevel }) => (
-  (level === 'All' || scopeLevel === level)
-    && name.toLowerCase().includes(queryStr)
-);
+const getFilterCallback = (level, queryStr) => ({ name, scopeLevel }) => {
+  const isLevelMatches = level === 'All' || scopeLevel === level;
+  const nameLowerCased = name.toLowerCase();
+  const queryStrLowerCased = queryStr.trim().toLowerCase();
+
+  return isLevelMatches && nameLowerCased.includes(queryStrLowerCased);
+};
 
 export default getFilterCallback;
