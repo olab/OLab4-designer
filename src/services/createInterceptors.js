1867426832b2bb94a4b6a53309e946359704a0a0
Oleg Dublyanin <oleg.dublyanin@itrexgroup.com>
2019-07-28 16:42:15 +0300

    OLAB-130: Update axios interceptors
    - Refactored interceptors and added descriptions
    - Added response interceptor to handle specific statuses from API
    
    Change-Id: I18df1cce467279d4000a13e578fd4a4ea376bd79

--- src/services/createInterceptors.js
+++ src/services/createInterceptors.js
@@ -1,24 +1,66 @@
 import store from '../store/store';
 import { ACTION_USER_AUTH_LOGOUT } from '../app/Login/action';
 
+/**
+ * Handle the 401 Auth error
+ * Remove stored token from local storage
+ *
+ * @param {Error} error
+ * @throws error
+ */
+const authResponseRejectInterceptor = (error) => {
+  if (error.response && error.response.status === 401) {
+    localStorage.removeItem('token');
+    store.dispatch(ACTION_USER_AUTH_LOGOUT());
+  }
+
+  throw error;
+};
+
+/**
+ * Handle response specific errors, when HTTP status returns 200
+ *
+ * @param {Object} response
+ * @returns {Object}
+ * @throws Error
+ */
+const errorSpecificResponseInterceptor = (response) => {
+  const { data: { error_code: errorCode, message } } = response;
+  if (errorCode > 400) {
+    throw new Error(message);
+  }
+
+  return response;
+};
+
+/**
+ * Set bearer token to headers
+ *
+ * @param {Object} config
+ * @returns {Object}
+ */
+const setAuthTokenRequestInterceptor = (config) => {
+  const token = localStorage.getItem('token');
+  if (token != null) {
+    config.headers.common.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
+
 const addInterceptors = (instance) => {
-  instance.interceptors.response.use(response => response, (error) => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      store.dispatch(ACTION_USER_AUTH_LOGOUT());
-    }
+  /**
+   * Request Interceptors
+   */
+  instance.interceptors.request.use(setAuthTokenRequestInterceptor, (error) => { throw error; });
 
-    throw error;
-  });
-
-  instance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token != null) {
-      config.headers.common.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  }, (error) => { throw error; });
+  /**
+   * Response Interceptors
+   */
+  instance.interceptors.response.use(
+    errorSpecificResponseInterceptor,
+    authResponseRejectInterceptor,
+  );
 
   return instance;
 };
